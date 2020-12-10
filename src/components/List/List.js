3cbb25474cfffdd0f06a48d6555b46d1f636261b
Dominika Jelonek <jelonekdominika@gmail.com>
2020-12-10 08:14:18 +0100

    Add 3 columns

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    source: PropTypes.string.isRequired,
+    columns: PropTypes.array,
+  };
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  };
+
   render() {
     return (
       <section className={styles.component}>
-        
-        <Hero />
+        <Hero titleText={this.props.title} image={this.props.source} />
+        <div className={styles.description}>{this.props.children}</div>
+
+        <div className={styles.columns}>
+          <Column minititle={this.props.columns[0]} />
+          <Column minititle={this.props.columns[1]} />
+          <Column minititle={this.props.columns[2]} />
+        </div>
       </section>
     );
   }
