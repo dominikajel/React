a85b6a78421866ff33e229e3d1a6bde12a33be25
Dominika Jelonek <jelonekdominika@gmail.com>
2020-12-11 09:11:56 +0100

    Add Card, Button, Icon functionalities

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,9 +4,15 @@ import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column';
 import { settings } from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator';
 
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  };
+
   static propTypes = {
     title: PropTypes.node.isRequired,
     image: PropTypes.string.isRequired,
@@ -18,16 +24,41 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   };
 
+  addColumn(title) {
+    this.setState((state) => ({
+      columns: [
+        ...state.columns,
+        {
+          key: state.columns.length
+            ? state.columns[state.columns.length - 1].key + 1
+            : 0,
+          title,
+          icon: 'list-alt',
+          cards: [],
+        },
+      ],
+    }));
+  }
+
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image} />
-        <div className={styles.description}>{this.props.description}</div>
+        <div className={styles.description}>
+          {ReactHtmlParser(this.props.description)}
+        </div>
 
         <div className={styles.columns}>
-          <Column columns={this.props.columns[0]} />
-          <Column columns={this.props.columns[1]} />
-          <Column columns={this.props.columns[2]} />
+          {this.state.columns.map(({ key, ...columnProps }) => (
+            <Column key={key} {...columnProps} />
+          ))}
+        </div>
+
+        <div className={styles.creator}>
+          <Creator
+            text={settings.columnCreatorText}
+            action={(title) => this.addColumn(title)}
+          />
         </div>
       </section>
     );
