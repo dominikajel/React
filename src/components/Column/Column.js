c3bb1c4c6d472c3f9bf3242680cd03e2f6edd6fe
Dominika Jelonek <jelonekdominika@gmail.com>
2020-12-18 16:26:41 +0100

    Add adding cards with redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 import { settings } from '../../data/dataStore';
-// import Creator from '../Creator/Creator';
+import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
 
 class Column extends React.Component {
@@ -11,6 +11,7 @@ class Column extends React.Component {
     title: PropTypes.string,
     cards: PropTypes.object,
     icon: PropTypes.element,
+    addCard: PropTypes.func, 
   };
 
   static defaultProps = {
@@ -18,7 +19,7 @@ class Column extends React.Component {
   };
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -34,14 +35,9 @@ class Column extends React.Component {
           ))}
         </div>
 
-        {/*
         <div className={styles.creator}>
-          <Creator
-            text={settings.cardCreatorText}
-            action={(title) => this.addColumn(title)}
-          />
+          <Creator text={settings.cardCreatorText} action={addCard} />
         </div>
-          */}
       </section>
     );
   }
