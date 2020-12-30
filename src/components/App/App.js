5f0c7c324ae7f220a0248343638c7c0825e86b81
Dominika Jelonek <jelonekdominika@gmail.com>
2020-12-30 08:26:56 +0100

    Add navigation - not working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,16 +5,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 
 const App = () => (
-  <div>
-    <MainLayout>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/info' component={Info} />
-        </Switch>
-      </BrowserRouter>
-    </MainLayout>
-  </div>
+  <MainLayout>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+      </Switch>
+    </BrowserRouter>
+  </MainLayout>
 );
 
 export default App;
